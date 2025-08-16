da32beb2e17abfc5d1afee24d16039bc32d2cfa7
JohannesFalk99 <johannesfalk99@gmail.com>
2025-08-16 11:00:24 +0200

    feat(core-sensors): add MAX31865 adapter

---
+++ include/utils/MAX31865Adapter.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "utils/ITemperatureSensor.h"
+#include <Adafruit_MAX31865.h>
+
+class MAX31865Adapter : public ITemperatureSensor {
+public:
+    MAX31865Adapter(uint8_t csPin) : sensor(csPin) {}
+    void begin() { sensor.begin(MAX31865_3WIRE); }
+    float readTemperature() override {
+        constexpr float RNOMINAL = 100.0f;
+        constexpr float RREF = 424.0f;
+        return sensor.temperature(RNOMINAL, RREF);
+    }
+    uint8_t readFault() { return sensor.readFault(); }
+    void clearFault() { sensor.clearFault(); }
+private:
+    Adafruit_MAX31865 sensor;
+};
