4506e1648efd55fe9b18564252d2396fc31c3886
JohannesFalk99 <johannesfalk99@gmail.com>
2025-08-16 11:02:13 +0200

    feat(web-core): add web server actions header

---
+++ include/WebServerActions.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <ArduinoJson.h>
+#include <ESPAsyncWebServer.h>
+class WebServerManager;
+namespace WebServerActions {
+    void handleControlUpdate(WebServerManager* mgr, AsyncWebSocketClient* client, JsonVariant data);
+    void handleGetHistory(WebServerManager* mgr, AsyncWebSocketClient* client, JsonVariant data);
+    void handleNotepadList(WebServerManager* mgr, AsyncWebSocketClient* client, JsonVariant data);
+    void handleNotepadLoad(WebServerManager* mgr, AsyncWebSocketClient* client, JsonVariant data);
+    void handleNotepadSave(WebServerManager* mgr, AsyncWebSocketClient* client, JsonVariant data);
+}
